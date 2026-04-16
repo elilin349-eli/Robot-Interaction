dca9b7908679967f6994fe46e8770176767a5233
JIAWEI XIONG <elilin349@gmail.com>
2026-04-16 13:00:37 +0800

    Update voice_robot.py

--- voice_robot.py
+++ voice_robot.py
@@ -49,7 +49,7 @@ if os.path.exists(env_path):
     except Exception:
         pass
 
-APP_ID = os.environ.get('IFLY_APP_ID', 'REMOVED')
+APP_ID = os.environ.get('IFLY_APP_ID', 'YOUR_APP_ID_HERE')
 API_KEY = os.environ.get('IFLY_API_KEY', 'REMOVED')
 API_SECRET = os.environ.get('IFLY_API_SECRET', 'MWNlODE0YzRhODEyYTFjNmMwNTVkZmFh')
 
